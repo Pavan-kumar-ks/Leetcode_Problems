e43f7d43ba5d859fc16ccdbc2c7516b3ef1914d6
Pavan Kumar KS <151180271+Pavan-kumar-ks@users.noreply.github.com>
2025-08-02 12:41:07 +0530

    Add files via upload

---
+++ Trees/BinaryTrees.java
@@ -0,0 +1,24 @@
+package pavan.Trees;
+import java.util.Scanner;
+public class BinaryTrees {
+    public BinaryTrees(){
+
+    }
+    private static class Node{
+        int val;
+        Node left;
+        Node right;
+        public Node(int v){
+            val = v;
+        }
+    }
+    private Node root;
+    public void insert(int v){
+        public void insert(Scanner scanner){
+            System.out.println("enter the node");
+            int val = scanner.nextInt();
+            root = new Node(val);
+            insert(scanner,root);
+        }
+    }
+}
